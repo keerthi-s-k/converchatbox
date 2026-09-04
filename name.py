435dec84f49899d406ff6f9a7c57b151de9310f7
keerthi-s-k <kurdekarkeerthi93@gmail.com>
2026-09-04 11:26:39 +0530

    Enhance chat responses for various user queries
    Added responses for additional user inputs.

--- name.py
+++ name.py
@@ -6,3 +6,15 @@ if a:
 		st.chat_message("ai").write("hello")
 	elif a.lower()=="bye":
 		st.chat_message("ai").write("goodbye")
+	elif a.lower()=="how are you":
+		st.chat_message("ai").write("im fine")
+	elif a.lower()=="where do you live":
+		st.chat_message("ai").write("i live in banglore")
+	elif a.lower()=="what is the time":
+		st.chat_message("ai").write("its 12 o'clock")
+	elif a.lower()=="can i get a ticket":
+		st.chat_message("ai").write("yes,here is your ticket")
+	elif a.lower()=="how was your day":
+		st.chat_message("ai").write("it was good")
+	else:
+		st.chat_message("ai").write("invalid command")
